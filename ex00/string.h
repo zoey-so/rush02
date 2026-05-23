31f9a4d14e6eb642e76deeeb5b6057a974b4b936
Przemysław Augustyniak <paugusty@student.42warsaw.pl>
2026-05-23 19:08:41 +0200

    Makefile, dictionaries, dict.[ch], string.[ch]

---
+++ ex00/string.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   string.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paugusty <paugusty@student.42warsaw.p      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/23 15:58:40 by paugusty          #+#    #+#             */
+/*   Updated: 2026/05/23 18:23:16 by paugusty         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRING_H
+# define STRING_H
+
+typedef struct s_string
+{
+	int		bufsize;
+	int		size;
+	char	*str;
+} t_string;
+
+t_string	*create_string(void);
+void		add_char(t_string *str, char ch);
+
+#endif
