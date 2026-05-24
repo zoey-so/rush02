882986e5f4449f15df086f7bce4f48b853e47b7e
Krzysztof Bartoszewski <kbartosz@c2r6s3.42warsaw.pl>
2026-05-24 15:38:36 +0200

    remove dict name validation from main

--- ex00/main.c
+++ ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: kbartosz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/23 13:57:34 by kbartosz          #+#    #+#             */
-/*   Updated: 2026/05/24 13:02:32 by kbartosz         ###   ########.fr       */
+/*   Updated: 2026/05/24 15:35:59 by kbartosz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,69 +15,53 @@
 #include "dict.h"
 #include "tool.h"
 
-void	convert_s(t_dict *dict, char *num_s, int num_len)
-int		load_dict(s_dict *d);
-/*
-int	ft_strlen(char *str);
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-*/
-
-int		validate_ref_dict_arg(char *dict_name)
-{
-	return (true);     // TODO should some validation be before calling open_dict()
-}
+void	convert_s(t_dict *dict, char *num_s, int num_len);
+int		open_dict(/*t_dict *d,*/ char *dist_path);
 
 // if only digits (have to be valid, positive integer)
-int	validate_num_arg(char *num_arg)
+// version not allowing leading zeroes:
+int	is_num_arg_valid(char *num_arg)
 {
 	int	i;
 
 	i = 0;
+	if (num_arg[i] == '0')
+		return (false);
 	while (num_arg[i])
 		if (num_arg[i] < '0' || num_arg[i] > '9')
+		{
+			ft_putstr("Error", STDERR_FILENO);
 			return (false);
+		}
 	return (true);
 }
 
-int	is_args_valid(int mode, char **argv)
-{
-	// TODO  validate_ref_dict_arg functions
-	if (mode == 2 && !is_ref_dict_arg_valid(argv[1])) // check if file exists ?? or...what?
-	{
-		ft_putstr("XD ??? error", STDERR_FILENO); // TODO WHAT ERROR MESSAGE HERE
-		return (false);
-	}
-	if (!is_num_arg_valid(argv[mode - 1]))
-	{
-		ft_putstr("Error", STDERR_FILENO);
-		return (false);
-	}
-	return (true);
-}
-
-int	main(int argc, char *argvp[])
+/**
+ * mode 1: only 1 argument: number
+ * mode 2: 1st argument: dict_path, 2nd argument: number
+ */
+int	main(int argc, char *argv[])
 {
 	int		mode;
-	s_dict	*dict;
+	t_dict	*dict;
+	char	*dict_path;
 
 	if (argc < 2 || argc > 3)
 		return (0);
 	if (argc == 3)
-		mode = 2; // 2 arguments: dict and num
+	{
+		mode = 2;
+		dict_path = argv[1];
+	}
 	else
-		mode = 1; // 1 argument only: num
-	if (!is_args_valid(mode, argv))
+	{
+		mode = 1;
+		dict_path = "numbers.dict";
+	}
+	if (!is_num_arg_valid(argv[mode]))
 		return (0);
-	// TODO if mode == 2 pass other dict file to open
-	dict = NULL;
-	if (!open_dict(dict))
+	(void) dict = create_dict(); // TODO change after open_dict will allow to take this
+	if (!open_dict(/*dict*,*/ dict_path))
 	{
 		ft_putstr("Dict Error\n", STDERR_FILENO);
 		return (0);
