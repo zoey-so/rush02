4791ccd83e579354eaee542218e4af10950679ba
zoey-so <xkokaxx@gmail.com>
2026-05-24 16:38:32 +0200

    convert string should compile

--- ex00/convert_string.c
+++ ex00/convert_string.c
@@ -1,5 +1,7 @@
 #include <unistd.h>
 #include "tool.h"
+#include "dict.h"
+#include <stdlib.h>
 
 int	search_exact(t_dict *dict, char *num_s, int num_len)
 {
@@ -34,10 +36,10 @@ void	put_digit(t_dict *dict, char c)
 	{
 		if (dict -> numlens[i] == 1)
 		{
-			if (dict -> *nums[i] == c)
+			if (dict -> nums[i][0] == c)
 			{
 				ft_putstr(dict -> strs[i], STDOUT_FILENO);
-				return (1);
+				return ;
 			}
 		}
 	}
@@ -55,7 +57,7 @@ void	put_100(t_dict *dict)
 	{
 		if (dict -> numlens[i] == 3)
 		{
-			if (!strncmp(dict -> nums[i], "100"))
+			if (!ft_strncmp(dict -> nums[i], "100", 3))
 				ft_putstr(dict -> strs[i], STDOUT_FILENO);
 		}
 	}
@@ -68,27 +70,68 @@ void	put_tens(t_dict *dict, char *num_s, int num_len)
 {
 	char	temp[2];
 
-	if (exact_match(dict, num_s, num_len))
+	if (search_exact(dict, num_s, num_len))
 		return ;
 	temp[0] = num_s[0];
 	temp[1] = '0';
-	(exact_match(dict, temp, 2));
+	search_exact(dict, temp, 2);
 	if (*(num_s + 1) != '0')
 		put_digit(dict, *(num_s + 1));
+}
+
+int	check_thousands(t_dict *dict, char *num_s, int num_len, int thousands)
+{
+	char	*temp;
+	int		i;
+
+	temp = (char *)malloc(sizeof(char) * (3 * thousands + num_len + 1));
+	if (!temp)
+		return (0);
+	i = -1;
+	while (++i < num_len)
+		temp[i] = num_s[i];
+	while (i < 3 * thousands + num_len)
+		temp[i++] = '0';
+	temp[i] = '\0';
+	if (search_exact(dict, temp, 3 * thousands + num_len))
+		i = 0;
+	free(temp);
+	if (!i)
+		return (1);
+	return (0);
+}
+
+
+int	check_hundreds(t_dict *dict, char *num_s)
+{
+	char	temp[4];
+
+	temp[0] = num_s[0];
+	temp[1] = '0';
+	temp[2] = '0';
+	temp[3] = '\0';
+	if (search_exact(dict, temp, 3))
+		return (1);
+	return (0);
 }
 
 // 1. search for exact match of hundred number
 // 2. if no exact print digit number and hundreds string
 // 3. if second char not 0 - go to handling tens function
 // 4. else print the 1-9
-void	put_hundreds(t_dict *dict, char *num_s, int num_len)
+int	put_hundreds(t_dict *dict, char *num_s, int num_len, int thousands)
 {
+	if (check_thousands(dict, num_s, num_len, thousands))
+		return (1);
 	if (search_exact(dict, num_s, num_len))
-		return ;
+		return (0);
 	if (num_len == 3)
 	{
-		put_digit(dict, num_s[0]);
-		put_100(dict);
+		if (*num_s == '1' || !check_hundreds(dict, num_s))
+		{
+			put_digit(dict, num_s[0]);
+			put_100(dict);
+		}
 		num_len--;
 		while (num_len > 0 && *(++num_s) == '0')
 			num_len--;
@@ -96,16 +139,32 @@ void	put_hundreds(t_dict *dict, char *num_s, int num_len)
 	if (num_len == 2)
 	{
 		put_tens(dict, num_s, num_len);
-		return ;
+		return (0);
 	}
 	else if (num_len == 1)
 		put_digit(dict, num_s[0]);
+	return (0);
 }
 
-void	put_thousands(t_dict *dict, char *num_s, int thousands)
+void	put_thousands(t_dict *dict, int thousands)
 {
+	char	*temp;
+	int		i;
 
+	if (thousands == 0)
+		return ;
+	temp = (char *)malloc(sizeof(char) * (3 * thousands + 2));
+	if (!temp)
+		return ;
+	temp[0] = '1';
+	i = 0;
+	while (++i < 3 * thousands + 1)
+		temp[i] = '0';
+	temp[i] = '\0';
+	search_exact(dict, temp, 3 * thousands + 1);
+	free(temp);
 }
+
 
 // 1. check if we have any exact match in dict eg 42: hello world
 // 2. if number is less than thousand handle it accordinly
@@ -119,26 +178,27 @@ void	convert_s(t_dict *dict, char *num_s, int num_len)
 	int		thousands;
 	int		tens;
 
-	if (num_len < 4)
-		put_hundreds(dict, num_s, num_len);
-	else
+	thousands = (num_len - 1) / 3;
+	while (thousands >= 0 && num_len)
 	{
-		thousands = (num_len - 1) / 3;
-		while (thousands > 0)
+		while (*num_s == '0' && (num_len - 1) / 3 == thousands)
 		{
-			while (*num_s == '0')
-			{
-				num_s++;
-				num_len--;
-			}
-			if ((search_exact(dict, num_s, num_len)))
-				return ;
-			tens = (num_len - 1) % 3;
-			put_hundreds(dict, num_s, tens + 1);
-			put_thousands(dict, num_s, thousands);
-			num_len -= 3;
-			num_s += 3;
-			thousands--;
+			num_s++;
+			num_len--;
 		}
+		if ((num_len - 1) / 3 != thousands)
+		{
+			thousands--;
+			continue ;
+		}
+		if ((search_exact(dict, num_s, num_len)))
+			return ;
+		tens = (num_len - 1) % 3;
+		if (!put_hundreds(dict, num_s, tens + 1, thousands))
+			put_thousands(dict, thousands);
+		num_len -= tens + 1;
+		num_s += tens + 1;
+		thousands--;
 	}
 }
+
