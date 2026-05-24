b738b2d7a23c154738944f238f9abec4d9553535
Krzysztof Bartoszewski <kbartosz@c2r6s3.42warsaw.pl>
2026-05-24 13:30:51 +0200

    norminette for tool.c

--- ex00/tool.c
+++ ex00/tool.c
@@ -6,7 +6,7 @@
 /*   By: paugusty <paugusty@student.42warsaw.p      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/23 19:15:45 by paugusty          #+#    #+#             */
-/*   Updated: 2026/05/24 13:16:45 by kbartosz         ###   ########.fr       */
+/*   Updated: 2026/05/24 13:29:50 by kbartosz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,24 +31,24 @@ void	copy(char *dest, char *src)
 	*dest = '\0';
 }
 
-void    ft_putstr(char *str, int fildes)
+void	ft_putstr(char *str, int fildes)
 {
-    write(fildes, str, _strlen(str));
+	write(fildes, str, _strlen(str));
 }
 
-int ft_strncmp(char *s1, char *s2, unsigned int n)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-    unsigned int    i;
-    int             r;
+	unsigned int	i;
+	int				r;
 
-    i = 0;
-    r = 0;
-    while (!r && i < n && (s1[i] || s2([i])))
-    {
-        r = (unsigned char) s1[i] - (unsigned char) s2[i];
-        if (r)
-            return (r);
-        i++;
-    }
-    return (r);
+	i = 0;
+	r = 0;
+	while (!r && i < n && (s1[i] || s2([i])))
+	{
+		r = (unsigned char) s1[i] - (unsigned char) s2[i];
+		if (r)
+			return (r);
+		i++;
+	}
+	return (r);
 }
